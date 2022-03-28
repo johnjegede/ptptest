2d33cd019f7940608d3ef8aa16e64ae78bec0607
John Jegede <jegede400@gmail.com>
2022-03-28 01:27:53 -0400

    change to makefile and master

--- environment/ptp_test/source/run_ptpd_master.sh
+++ environment/ptp_test/source/run_ptpd_master.sh
@@ -6,6 +6,6 @@ IP_DST3="$5"
 I="$6"
 PORT1="$7"
 PORT2="$8"
-sudo ~/ptpd/src/ptpd2 -i ${I} -E -M -U -g -u ${IP_DST1},${IP_DST2},${IP_DST3} -r ${INT} -j ${PORT1} -z ${PORT2}
+sudo ~/ptpd/src/ptpd2 -i ${I} -E -M -U -g -u ${IP_DST1} -r ${INT} -j ${PORT1} -z ${PORT2}
 sleep ${TIME}
 sudo kill -9 $(pgrep -f ptpd)
